3abb33eeb5a55f32bb9cfcdf6e2159c75e103419
lann95081 <lann95081@gmail.com>
2022-11-16 08:10:31 +0700

    up

--- ss1_database_introduction/exercise/student_management.sql
+++ ss1_database_introduction/exercise/student_management.sql
@@ -1,15 +1,18 @@
-create database if not exists `my_database1`;
-use my_database1;
+create database if not exists student_management;
+use student_management;
+
 create table student(
   id int primary key, 
   `name` varchar(50) not null, 
   age int, 
   country varchar(50)
 );
+
 create table class(
   id int primary key, 
   `name` varchar(50)
 );
+
 create table teacher(
   id int primary key, 
   `name` varchar(50), 
