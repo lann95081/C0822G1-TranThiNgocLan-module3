f78aa5e46c435a2e973b97a00395458b38d13e54
lann95081 <lann95081@gmail.com>
2022-11-17 19:57:49 +0700

    up

--- case_study_database/furama_query.sql
+++ case_study_database/furama_query.sql
@@ -1,11 +1,13 @@
 use `furama_create_table`;
 
 -- 2.Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
-select * from nhan_vien 
-where ho_ten like 'H%' or 'T%' or 'K%' and length(ho_ten) <= 15;
+select nhan_vien.ma_nhan_vien, nhan_vien.ho_ten
+from nhan_vien 
+where ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%' and length(ho_ten) <= 15;
 
 -- 3.Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
-select * from khach_hang 
+select khach_hang.ma_khach_hang, khach_hang.ho_ten
+from khach_hang 
 where (year(curdate()) - year(ngay_sinh) between 18 and 50) and (dia_chi like '%Đà Nẵng' or dia_chi like '%Quảng Trị');
 
 -- 4.Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần.
