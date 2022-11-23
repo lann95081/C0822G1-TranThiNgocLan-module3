f4a467e7480a4a4ab7cf297a25d133449485df24
lann95081 <lann95081@gmail.com>
2022-11-23 15:46:52 +0700

    up

--- case_study_database/furama_query.sql
+++ case_study_database/furama_query.sql
@@ -26,15 +26,14 @@ order by so_lan_dat_phong;
 -- Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng.
 -- (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
 select kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc,
-(ifnull(dv.chi_phi_thue,0) + sum(ifnull(dvdk.gia,0) * ifnull(hdct.so_luong,0))) as tong_tien
+ifnull(dv.chi_phi_thue,0) + sum(ifnull(dvdk.gia,0) * ifnull(hdct.so_luong,0)) as tong_tien
 from khach_hang kh
 left join loai_khach lk on lk.ma_loai_khach = kh.ma_loai_khach
 left join hop_dong hd on hd.ma_khach_hang = kh.ma_khach_hang
 left join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
 left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
 left join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
-group by ma_hop_dong, ma_khach_hang
-order by ma_khach_hang asc, ma_hop_dong desc;
+group by ma_hop_dong;
 
 -- 6.Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
 -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
@@ -98,7 +97,8 @@ group by ma_hop_dong;
 
 -- 11.Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach
  -- là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
- select dvdk.ma_dich_vu_di_kem,dvdk.ten_dich_vu_di_kem from dich_vu_di_kem dvdk
+ select dvdk.ma_dich_vu_di_kem,dvdk.ten_dich_vu_di_kem 
+ from dich_vu_di_kem dvdk
  join hop_dong_chi_tiet hdct on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
  join hop_dong hd on hd.ma_hop_dong = hdct.ma_hop_dong
  join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_hang
@@ -126,7 +126,7 @@ group by hd.ma_hop_dong;
 -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
 select dvdk.ma_dich_vu_di_kem, dvdk.ten_dich_vu_di_kem, sum(hdct.so_luong) as so_luong_dich_vu_di_kem
 from dich_vu_di_kem dvdk
-join hop_dong_chi_tiet hdct on hdct.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+join hop_dong_chi_tiet hdct on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
 group by ma_dich_vu_di_kem
 having so_luong_dich_vu_di_kem = (
 	select sum(hdct.so_luong) as sl
@@ -138,7 +138,7 @@ having so_luong_dich_vu_di_kem = (
 -- 14.Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
 -- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung 
 -- (được tính dựa trên việc count các ma_dich_vu_di_kem).
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+set sql_mode = (select replace(@@sql_mode,'only_full_group_by',''));
 select hdct.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem, count(hdct.ma_dich_vu_di_kem) as so_lan_su_dung
 from dich_vu dv
 join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
@@ -185,7 +185,7 @@ from (select kh.ma_khach_hang, sum(ifnull(dv.chi_phi_thue, 0) + ifnull(hdct.so_l
 	left join loai_khach lk on lk.ma_loai_khach = kh.ma_loai_khach
 	where year(hd.ngay_lam_hop_dong) = 2021 and lk.ten_loai_khach = 'Platinium'
 	group by ma_khach_hang
-	having tong_tien > 10000000) as temp
+	having tong_tien > 10000000) as nan
 );
 
 -- 18.Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
@@ -208,7 +208,7 @@ select * from (select dvdk.ma_dich_vu_di_kem
 	join hop_dong hd on hd.ma_hop_dong = hdct.ma_hop_dong
 	where year(hd.ngay_lam_hop_dong) = 2020
 	group by ma_dich_vu_di_kem
-	having sum(hdct.so_luong) > 10) as temp);
+	having sum(hdct.so_luong) > 10) as nan);
 
 select *
 from dich_vu_di_kem;
@@ -220,4 +220,16 @@ from nhan_vien
 union all
 select ma_khach_hang as id, ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi
 from khach_hang;
+
+-- 21.Tạo khung nhìn có tên là v_nhan_vien để lấy được thông tin của tất cả các nhân viên có địa chỉ là “Hải Châu” 
+-- và đã từng lập hợp đồng cho một hoặc nhiều khách hàng bất kì với ngày lập hợp đồng là “12/12/2019”.
+create view v_nhan_vien as 
+select nv.ma_nhan_vien, nv.ho_ten, nv.dia_chi, hd.ngay_lam_hop_dong
+from nhan_vien nv
+join hop_dong hd on hd.ma_nhan_vien = nv.ma_nhan_vien
+where nv.dia_chi like '%Hải Châu%' and hd.ngay_lam_hop_dong = '12/12/2019';
+
+select * from v_nhan_vien;
+
+drop view v_nhan_vien;
 
